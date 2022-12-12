7b46d84869d4e766ab607a060730bed58a6da8b6
clanbc <44944378+clanbc@users.noreply.github.com>
2022-12-12 15:16:00 +0000

    Add complete day-6 (#8)

---
+++ aoc/day6/solution-2.py
@@ -0,0 +1,25 @@
+def check_unique(str):
+    for i in range(len(str)):
+        for j in range(i + 1, len(str)):
+            if str[i] == str[j]:
+                return False
+    return True
+
+
+def main():
+    with open("input.txt") as f:
+        content = f.read()
+
+        char_total = 14
+        for char in content:
+            char_total += 1
+            potential_marker = content[(char_total - 14) : char_total]  # noqa:E203
+
+            marker = check_unique(potential_marker)
+            if marker:
+                print(f"marker is {potential_marker} & processed chars is {char_total}")
+                break
+
+
+if __name__ == "__main__":
+    main()
